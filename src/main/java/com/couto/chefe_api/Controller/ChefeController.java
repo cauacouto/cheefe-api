a267d8ddf2ae90df5ad7dca039e89b31fb354018
cauacouto <cauacouto022@gmail.com>
2026-04-05 19:24:02 -0300

    feat:criar controller

--- src/main/java/com/couto/chefe_api/Controller/ChefeController.java
+++ src/main/java/com/couto/chefe_api/Controller/ChefeController.java
@@ -3,13 +3,14 @@ package com.couto.chefe_api.Controller;
 import com.couto.chefe_api.Dtos.ChefeRequestDto;
 import com.couto.chefe_api.Dtos.ChefeResponseDto;
 import com.couto.chefe_api.Services.ChefeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/chefe")
@@ -20,8 +21,29 @@ public class ChefeController {
     public ChefeController(ChefeService service) {
         this.service = service;
     }
-@PostMapping
+    @PostMapping
     public ResponseEntity<ChefeResponseDto> salvarChefe(@RequestBody @Validated ChefeRequestDto dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.salvarChefe(dto));
     }
+    @GetMapping
+    public ResponseEntity <Page<ChefeResponseDto>> listarChefes(Pageable pageable){
+        return ResponseEntity.ok().body(service.mostrarChefes(pageable));
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<ChefeResponseDto> atualizar(@RequestBody ChefeRequestDto dto, @PathVariable UUID id){
+        var chefeAtualizado = service.atualizarDados(dto,id);
+        return ResponseEntity.ok(chefeAtualizado);
+
+    }
+    @PutMapping("{id}/inativar")
+    public ResponseEntity<Void> iniativar(@PathVariable UUID id){
+        service.inativar(id);
+        return ResponseEntity.noContent().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable UUID id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
