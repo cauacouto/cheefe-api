b8fb2cc4e34a307393ded735e4296b39cd180715
cauacouto <cauacouto022@gmail.com>
2026-04-05 12:53:31 -0300

    feat:criar classe chefe model

---
+++ src/main/java/com/couto/chefe_api/domin/ChefeModel.java
@@ -0,0 +1,47 @@
+package com.couto.chefe_api.domin;
+
+import com.couto.chefe_api.enums.Especialidades;
+import com.couto.chefe_api.enums.Senioridade;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "chefe")
+public class ChefeModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
+    private UUID id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String nacionalidade;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidades especialidade;
+
+    @NotNull
+    private int anosDeExperiencia;
+
+    @Enumerated(EnumType.STRING)
+    private Senioridade senioridade;
+@Column(nullable = false)
+    private boolean disponivel = true;
+    @NotBlank
+    private String idiomas;
+}
