c9915369842a8a7a936e1fcdcfff57ea7c1b1c96
cauacouto <cauacouto022@gmail.com>
2026-04-05 19:26:32 -0300

    update:adicionar nova query

--- src/main/java/com/couto/chefe_api/repositorys/repositoryChefe.java
+++ src/main/java/com/couto/chefe_api/repositorys/repositoryChefe.java
@@ -3,7 +3,11 @@ package com.couto.chefe_api.repositorys;
 import com.couto.chefe_api.domin.ChefeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface repositoryChefe extends JpaRepository<ChefeModel, UUID> {
+
+
+    List<ChefeModel> findByAtivoTrue();
 }
