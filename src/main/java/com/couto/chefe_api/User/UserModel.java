5f1b6538ba6c24a4984a57a8870197c42a209d98
cauacouto <cauacouto022@gmail.com>
2026-04-06 10:37:13 -0300

    feat: implementar UserDetails na entidade UserModel e adicionar relacionamentos com Endereço e Agendamento

--- src/main/java/com/couto/chefe_api/User/UserModel.java
+++ src/main/java/com/couto/chefe_api/User/UserModel.java
@@ -1,13 +1,18 @@
 package com.couto.chefe_api.User;
 
-import com.couto.chefe_api.enums.TipoDeResidencia;
+import com.couto.chefe_api.domin.Agendamento;
+import com.couto.chefe_api.domin.Endereço;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -24,11 +29,52 @@ public class UserModel {
     private UUID id ;
     @NotBlank
     private String nome;
-    @NotBlank
-    private String logradouro;
-    @NotBlank
-    private String cidade;
-    private TipoDeResidencia tipoDeResidencia;
-    @NotBlank
-    private String complemento;
+
+    private String email;
+
+    private String password;
+
+    private String telefone;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Endereço> endereços;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Agendamento> agendamento;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
