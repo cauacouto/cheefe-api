34644d84d7d912ba2f2ab9e654b8165677179e80
cauacouto <cauacouto022@gmail.com>
2026-04-06 10:35:56 -0300

    feat: adicionar entidade Agendamento

---
+++ src/main/java/com/couto/chefe_api/domin/Agendamento.java
@@ -0,0 +1,46 @@
+package com.couto.chefe_api.domin;
+
+import com.couto.chefe_api.User.UserModel;
+import com.couto.chefe_api.enums.StatusAgendamento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "agendamento")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Agendamento  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserModel userModel;
+
+    @ManyToMany
+    @JoinTable(
+            name = "agedamento_chefe",
+            joinColumns = @JoinColumn(name = "agendamento_id"),
+            inverseJoinColumns = @JoinColumn(name = "chefe_id")
+    )
+    private List<ChefeModel> chefeModels;
+
+    @Column(nullable = false)
+    private LocalDateTime datahora;
+
+    private StatusAgendamento status = StatusAgendamento.PENDENDE;
+    @CreationTimestamp
+    private LocalDateTime criado;
+}
