5da8495a10b215281287e497ff1f4a953b44f723
cauacouto <cauacouto022@gmail.com>
2026-04-06 23:07:33 -0300

    feat: add findByEmail method to UsuarioRepository for user retrieval by email

--- src/main/java/com/couto/chefe_api/User/UsuarioRepository.java
+++ src/main/java/com/couto/chefe_api/User/UsuarioRepository.java
@@ -2,7 +2,14 @@ package com.couto.chefe_api.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<UserModel, UUID> {
+
+
+    Optional<UserModel> findByEmail(String email);
+
+
+
 }
