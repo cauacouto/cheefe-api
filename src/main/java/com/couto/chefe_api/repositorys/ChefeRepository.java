b32defc5114fab68c454988521e9822856513d2e
cauacouto <cauacouto022@gmail.com>
2026-04-06 23:06:48 -0300

    refactor: rename repositoryChefe to ChefeRepository and add findByDisponivel method

--- src/main/java/com/couto/chefe_api/repositorys/ChefeRepository.java
+++ src/main/java/com/couto/chefe_api/repositorys/ChefeRepository.java
@@ -4,10 +4,15 @@ import com.couto.chefe_api.domin.ChefeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface repositoryChefe extends JpaRepository<ChefeModel, UUID> {
+public interface ChefeRepository extends JpaRepository<ChefeModel, UUID> {
 
 
     List<ChefeModel> findByAtivoTrue();
+
+
+
+    List<ChefeModel> findByDisponivel();
 }
