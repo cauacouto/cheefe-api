83d4275411074ae8237b5b414cb72f6ed15c73fb
cauacouto <cauacouto022@gmail.com>
2026-04-09 14:28:41 -0300

    feat: add AgendamentoController for managing agendamentos

---
+++ src/main/java/com/couto/chefe_api/Controller/AgendamentoController.java
@@ -0,0 +1,26 @@
+package com.couto.chefe_api.Controller;
+
+import com.couto.chefe_api.Dtos.AgendamentoResponseDto;
+import com.couto.chefe_api.Dtos.AgendamentoResquestDto;
+import com.couto.chefe_api.Services.agendamentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/agendamento")
+public class AgendamentoController {
+
+    private final agendamentoService service;
+
+    public AgendamentoController(agendamentoService service) {
+        this.service = service;
+    }
+@PostMapping
+    public ResponseEntity<AgendamentoResponseDto> criarAgendamento(@RequestBody AgendamentoResquestDto dto,String email){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criarAgendamento(dto,email));
+    }
+}
