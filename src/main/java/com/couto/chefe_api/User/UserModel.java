3e0e09d836335496a56dfa4254f48d3a7717c6cb
cauacouto <cauacouto022@gmail.com>
2026-04-09 14:27:29 -0300

    refactor: simplify UserModel by removing UserDetails implementation and updating address mapping

--- src/main/java/com/couto/chefe_api/User/UserModel.java
+++ src/main/java/com/couto/chefe_api/User/UserModel.java
@@ -1,17 +1,14 @@
 package com.couto.chefe_api.User;
 
 import com.couto.chefe_api.domin.Agendamento;
-import com.couto.chefe_api.domin.Endereço;
+import com.couto.chefe_api.domin.EnderecoModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,7 +18,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserModel implements UserDetails {
+public class UserModel  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,45 +33,10 @@ public class UserModel implements UserDetails {
 
     private String telefone;
 
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
-    private List<Endereço> endereços;
+    @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL)
+    private List<EnderecoModel> endereco;
 
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "usuario",cascade = CascadeType.ALL)
     private List<Agendamento> agendamento;
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
