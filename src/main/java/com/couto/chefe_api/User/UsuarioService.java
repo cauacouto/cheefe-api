59e37260180ecbea9a4c27530eabd83824a82e70
cauacouto <cauacouto022@gmail.com>
2026-04-13 21:49:08 -0300

    update: retira peek add forEach

--- src/main/java/com/couto/chefe_api/User/UsuarioService.java
+++ src/main/java/com/couto/chefe_api/User/UsuarioService.java
@@ -2,17 +2,21 @@ package com.couto.chefe_api.User;
 
 import com.couto.chefe_api.Dtos.UserRequestDto;
 import com.couto.chefe_api.Dtos.UserResponseDto;
+import com.couto.chefe_api.Excepitons.UsuarioException;
 import com.couto.chefe_api.Mapper.UsuarioMapper;
 import com.couto.chefe_api.domin.EnderecoModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UsuarioService {
 
-private final UsuarioRepository repository;
-private  final UsuarioMapper UserMapper;
+    private final UsuarioRepository repository;
+    private  final UsuarioMapper UserMapper;
 
 
     public UsuarioService(UsuarioRepository repository, UsuarioMapper userMapper) {
@@ -26,12 +30,32 @@ private  final UsuarioMapper UserMapper;
 
         List<EnderecoModel> enderecos = dto.endereco().stream()
                 .map(UserMapper::toModel)
-                .peek(e -> e.getTipoDeResidencia().validar(e))
                 .toList();
+
+        enderecos.forEach(e -> {
+            e.getTipoDeResidencia().validar(e);
+            e.setUsuario(usuario);
+        });
 
         usuario.setEndereco(enderecos);
 
         var salvar = repository.save(usuario);
         return UserMapper.toDto(salvar);
     }
+
+    public UserResponseDto atualizarUsuario(UserRequestDto dto, UUID id){
+        UserModel user = repository.findById(id).orElseThrow(UsuarioException::new);
+        UserMapper.toModel(dto);
+        var salvar = repository.save(user);
+        return UserMapper.toDto(salvar);
+    }
+
+    public Page<UserResponseDto> listarUsuarios(Pageable pageable){
+        return repository.findAll(pageable)
+                .map(UserMapper::toDto);
+
+
+    }
+
+
 }
