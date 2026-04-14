e04c0adedfd4b1b35a042f7212de2f9bc924af59
cauacouto <cauacouto022@gmail.com>
2026-04-13 21:47:53 -0300

    update: adicionar List endereço request

--- src/main/java/com/couto/chefe_api/Dtos/UserRequestDto.java
+++ src/main/java/com/couto/chefe_api/Dtos/UserRequestDto.java
@@ -1,7 +1,15 @@
 package com.couto.chefe_api.Dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
-public record UserRequestDto(String nome, String email, String password, String telefone, List<EnderecoRequestDto> endereco
+public record UserRequestDto(@NotBlank String nome,
+                            @NotBlank String email,
+                             @NotBlank String password,
+                            @NotBlank String telefone,
+                            @NotNull @NotEmpty List<EnderecoRequestDto> endereco
                              ) {
 }
