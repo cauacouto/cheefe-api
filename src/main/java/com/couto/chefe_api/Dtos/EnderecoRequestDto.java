dd60d88e361ad3c0a87ee58a0b1904d71e9ea5a9
cauacouto <cauacouto022@gmail.com>
2026-04-13 21:46:39 -0300

    update: adicionar string bairro

--- src/main/java/com/couto/chefe_api/Dtos/EnderecoRequestDto.java
+++ src/main/java/com/couto/chefe_api/Dtos/EnderecoRequestDto.java
@@ -2,6 +2,6 @@ package com.couto.chefe_api.Dtos;
 
 import com.couto.chefe_api.enums.TipoDeResidencia;
 
-public record EnderecoRequestDto(String rua, String numero, String complemento, String cep, String cidade, String estado,
+public record EnderecoRequestDto(String rua, String numero, String complemento, String cep, String cidade, String bairro,String estado,
                                  TipoDeResidencia tipoDeResidencia) {
 }
