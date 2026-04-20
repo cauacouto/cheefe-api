c95273614394ff161413e623da70e1be8050eedc
cauacouto <cauacouto022@gmail.com>
2026-04-20 00:23:46 -0300

    feat: criar endpoints listaAgendamentoChefes, ListAgendamentoUsuario

--- src/main/java/com/couto/chefe_api/Controller/AgendamentoController.java
+++ src/main/java/com/couto/chefe_api/Controller/AgendamentoController.java
@@ -2,13 +2,18 @@ package com.couto.chefe_api.Controller;
 
 import com.couto.chefe_api.Dtos.AgendamentoResponseDto;
 import com.couto.chefe_api.Dtos.AgendamentoResquestDto;
+import com.couto.chefe_api.Dtos.ListaAgendamentoChefeDto;
+import com.couto.chefe_api.Dtos.ListaAgendamentoUsuarioDto;
 import com.couto.chefe_api.Services.agendamentoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/agendamento")
+@RequestMapping("/agendamentos")
 public class AgendamentoController {
 
     private final agendamentoService service;
@@ -19,5 +24,21 @@ public class AgendamentoController {
 @PostMapping
     public ResponseEntity<AgendamentoResponseDto> criarAgendamento(@RequestBody AgendamentoResquestDto dto,@RequestParam String email){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.criarAgendamento(dto,email));
+    }
+
+    @GetMapping("/chefe/{chefeId}")
+    public ResponseEntity<List<ListaAgendamentoChefeDto>> listarAgendamentoChefes(@PathVariable UUID chefeId){
+        return ResponseEntity.ok(service.listarAgendamentosChefe(chefeId));
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<ListaAgendamentoUsuarioDto>> listarAgedamentoUsuarios(@PathVariable UUID usuarioId){
+     return ResponseEntity.ok(service.listarAgendamentosUsuario(usuarioId));
+    }
+
+    @PatchMapping("{id}/confirmar")
+    public ResponseEntity<Void> confirmarAgendamento(@PathVariable Long id){
+     service.confirmarAgendamento(id);
+     return ResponseEntity.ok().build();
     }
 }
