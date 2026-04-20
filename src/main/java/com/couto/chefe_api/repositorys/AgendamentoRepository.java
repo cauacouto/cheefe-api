01ba58fe48114df56c838efd445e7b4116916a2a
cauacouto <cauacouto022@gmail.com>
2026-04-20 00:22:34 -0300

    update: adicionar query findallByChefe, e findALLUsuarioId

--- src/main/java/com/couto/chefe_api/repositorys/AgendamentoRepository.java
+++ src/main/java/com/couto/chefe_api/repositorys/AgendamentoRepository.java
@@ -2,6 +2,19 @@ package com.couto.chefe_api.repositorys;
 
 import com.couto.chefe_api.domin.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento,Long> {
+
+
+    List<Agendamento> findAllByUsuario_Id(UUID usuario);
+
+    @Query("SELECT a FROM Agendamento a JOIN a.chefes c WHERE c.id = :chefeId")
+    List<Agendamento> findAllByChefe(@Param("chefeId") UUID chefeId);
+
+
 }
