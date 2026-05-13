6762431ca73aa9f70653cf83f3a89f1ff3640737
cauacouto <cauacouto022@gmail.com>
2026-05-13 11:48:21 -0300

    feat: update PratosMapper to include mappings for ListarPratosChefes and adjust existing DTO mappings

--- src/main/java/com/couto/chefe_api/Mapper/PratosMapper.java
+++ src/main/java/com/couto/chefe_api/Mapper/PratosMapper.java
@@ -2,15 +2,21 @@ package com.couto.chefe_api.Mapper;
 
 import com.couto.chefe_api.Dtos.CadastraPratosDtoRequest;
 import com.couto.chefe_api.Dtos.CadastratPratosDtoResponse;
+import com.couto.chefe_api.Dtos.ListarPratosChefes;
 import com.couto.chefe_api.domin.Pratos;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PratosMapper {
 
+    @Mapping(source = "nomePrato", target = "nomePrato")
+    Pratos toModel(CadastraPratosDtoRequest dto);
 
-    CadastratPratosDtoResponse toDto (Pratos model);
+    @Mapping(source = "chefeModel.nome", target = "nomeChefe")
+    @Mapping(source = "nomePrato", target = "nomePrato") // mesmo nome agora
+    CadastratPratosDtoResponse toDto(Pratos model);
 
-    Pratos toModel (CadastraPratosDtoRequest dto);
-
+    @Mapping(source = "chefeModel.nome", target = "nomeChefe")
+    ListarPratosChefes toDtoLista(Pratos model);
 }
