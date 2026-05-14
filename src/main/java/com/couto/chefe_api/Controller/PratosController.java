3d565a6edf8a4790a3325169d9901c2742e20be7
cauacouto <cauacouto022@gmail.com>
2026-05-13 23:41:57 -0300

    feat criar PratosController

---
+++ src/main/java/com/couto/chefe_api/Controller/PratosController.java
@@ -0,0 +1,54 @@
+package com.couto.chefe_api.Controller;
+
+import com.couto.chefe_api.Dtos.CadastraPratosDtoRequest;
+import com.couto.chefe_api.Dtos.CadastratPratosDtoResponse;
+import com.couto.chefe_api.Dtos.ListarPratosChefes;
+import com.couto.chefe_api.Services.PratosService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/pratos")
+public class PratosController {
+
+   private final PratosService pratosService;
+
+
+    @PostMapping("/pratos/{id}")
+    public ResponseEntity<CadastratPratosDtoResponse> cadastroPratos(@RequestBody CadastraPratosDtoRequest dtoRequest, @PathVariable UUID id){
+        CadastratPratosDtoResponse response = pratosService.cadastrarPratos(dtoRequest,id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+
+    @GetMapping("/chefe/{chefeId}")
+    public ResponseEntity<List<ListarPratosChefes>> listaPorChefe(@PathVariable UUID chefeId){
+        return ResponseEntity.ok(pratosService.listaPratos(chefeId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListarPratosChefes>> listarTodos() {
+        return ResponseEntity.ok(pratosService.listaPratos(null));
+    }
+//
+//    // Rota para o chefe autenticado ver os próprios pratos
+//    @GetMapping("/pratos/meus-pratos")
+//    public ResponseEntity<List<ListarPratosChefes>> meusPratos(@AuthenticationPricipal, UserDetais usuariologado) {
+  //  String email = usuarioLogado.getUsername()
+    // chefe chefe = cheferepository.findyByemail(email)
+    //  lança exececao
+//        return ResponseEntity.ok(pratosService.listaPratos(chefe.getId));
+//    }
+
+    @PutMapping
+    public ResponseEntity<CadastratPratosDtoResponse> updatePratos(@RequestBody CadastraPratosDtoRequest dto, Integer id){
+        CadastratPratosDtoResponse response = pratosService.updatePratos(dto,id);
+        return ResponseEntity.ok().body(response);
+    }
+}
