a9821afb90d570b29b3d67234404cba3afcd6cd9
cauacouto <cauacouto022@gmail.com>
2026-05-14 10:37:48 -0300

    update: depedencies cloudinary

--- src/main/java/com/couto/chefe_api/repositorys/PatrosRepository.java
+++ src/main/java/com/couto/chefe_api/repositorys/PatrosRepository.java
@@ -1,7 +1,6 @@
 package com.couto.chefe_api.repositorys;
 
 import com.couto.chefe_api.domin.Pratos;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
