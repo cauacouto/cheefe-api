205ba4a4b246bd4ed731d6a86a06ab47f3c12be1
cauacouto <cauacouto022@gmail.com>
2026-05-15 22:28:36 -0300

    feat: refactor UsuarioService to include password encryption and add user retrieval and deletion methods

--- src/main/java/com/couto/chefe_api/User/UsuarioService.java
+++ src/main/java/com/couto/chefe_api/User/UsuarioService.java
@@ -5,48 +5,45 @@ import com.couto.chefe_api.Dtos.UserResponseDto;
 import com.couto.chefe_api.Excepitons.UsuarioException;
 import com.couto.chefe_api.Mapper.UsuarioMapper;
 import com.couto.chefe_api.domin.EnderecoModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository repository;
     private  final UsuarioMapper UserMapper;
+    private final PasswordEncoder passwordEncoder;
 
 
-    public UsuarioService(UsuarioRepository repository, UsuarioMapper userMapper) {
-        this.repository = repository;
-        UserMapper = userMapper;
-
-    }
-
-    public UserResponseDto salvarUsuario(UserRequestDto dto){
-        UserModel usuario = UserMapper.toModel(dto);
-
-        List<EnderecoModel> enderecos = dto.endereco().stream()
-                .map(UserMapper::toModel)
-                .toList();
-
-        enderecos.forEach(e -> {
-            e.getTipoDeResidencia().validar(e);
-            e.setUsuario(usuario);
-        });
-
-        usuario.setEndereco(enderecos);
-
-        var salvar = repository.save(usuario);
-        return UserMapper.toDto(salvar);
-    }
 
     public UserResponseDto atualizarUsuario(UserRequestDto dto, UUID id){
         UserModel user = repository.findById(id).orElseThrow(UsuarioException::new);
-        UserMapper.toModel(dto);
-        var salvar = repository.save(user);
+
+        var senhaCriptografada = passwordEncoder.encode(dto.password());
+        user.setPassword(senhaCriptografada);
+        user.setNome(dto.nome());
+        user.setEmail(dto.email());
+        user.setTelefone(dto.telefone());
+
+         List<EnderecoModel> endecos = dto.endereco().stream()
+                 .map(UserMapper::toModel)
+                 .toList();
+
+         endecos.forEach(e ->{
+             e.getTipoDeResidencia().validar(e);
+             e.setUsuario(user);
+         });
+         user.setEndereco(endecos);
+
+         var salvar = repository.save(user);
         return UserMapper.toDto(salvar);
     }
 
@@ -55,6 +52,16 @@ public class UsuarioService {
                 .map(UserMapper::toDto);
 
 
+    }
+
+    public UserResponseDto buscarPorId(UUID id){
+        UserModel user = repository.findById(id)
+                .orElseThrow(UsuarioException::new);
+        return UserMapper.toDto(user);
+    }
+
+    public void deletar(UUID id){
+        repository.deleteById(id);
     }
 
 
