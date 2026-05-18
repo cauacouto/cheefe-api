fcef824d452fcbaac28b85f8c250a0809df3981d
cauacouto <cauacouto022@gmail.com>
2026-05-17 22:19:48 -0300

    feat: add endpoints to retrieve, list, and delete users by ID in UsuarioController

--- src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
+++ src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
@@ -4,10 +4,13 @@ import com.couto.chefe_api.Dtos.UserRequestDto;
 import com.couto.chefe_api.Dtos.UserResponseDto;
 import com.couto.chefe_api.User.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,5 +27,22 @@ public class UsuarioController {
         return ResponseEntity.ok().body(update);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> buscaPorId(@PathVariable UUID id){
+        var buscarId = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok().body(buscarId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDto>> listarUsuarios(Pageable page){
+     Page<UserResponseDto> users =  usuarioService.listarUsuarios(page);
+     return ResponseEntity.ok().body(users);
+
+    }
+    @DeleteMapping("/{id}")
+public ResponseEntity<Void> deletar (@PathVariable UUID id){
+        usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+}
 
 }
