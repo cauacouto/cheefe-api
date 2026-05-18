34e996e128247879ac57e68e008f9bdba890a019
cauacouto <cauacouto022@gmail.com>
2026-05-17 22:19:38 -0300

    feat: implement cadastroPratos and meusPratos methods with authentication

--- src/main/java/com/couto/chefe_api/Controller/PratosController.java
+++ src/main/java/com/couto/chefe_api/Controller/PratosController.java
@@ -3,10 +3,18 @@ package com.couto.chefe_api.Controller;
 import com.couto.chefe_api.Dtos.CadastraPratosDtoRequest;
 import com.couto.chefe_api.Dtos.CadastratPratosDtoResponse;
 import com.couto.chefe_api.Dtos.ListarPratosChefes;
+import com.couto.chefe_api.Excepitons.ChefeEception;
 import com.couto.chefe_api.Services.PratosService;
+import com.couto.chefe_api.domin.ChefeModel;
+import com.couto.chefe_api.repositorys.ChefeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,17 +25,19 @@ import java.util.UUID;
 public class PratosController {
 
    private final PratosService pratosService;
+   private  final ChefeRepository chefeRepository;
+
 
                                                                                                                                                   //adicionar UserDeails
-//    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    public ResponseEntity<CadastratPratosDtoResponse> cadastroPratos(@RequestPart("dados") CadastraPratosDtoRequest dto, @RequestParam MultipartFile foto,){
-//
-//        ChefeModel chefe = chefeRepository.findByEmail(usuarioLogado.getUsername())
-//                .orElseThrow(ChefeEception::new);
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(pratosService.cadastrarPratos(dto, foto, chefe.getId()));
-//    }
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CadastratPratosDtoResponse> cadastroPratos(@RequestPart("dados") CadastraPratosDtoRequest dto, @RequestParam MultipartFile foto, @AuthenticationPrincipal UserDetails usuarioLogado){
+
+        ChefeModel chefe = chefeRepository.findByEmail(usuarioLogado.getUsername())
+                .orElseThrow(ChefeEception::new);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pratosService.cadastrarPratos(dto, foto, chefe.getId()));
+    }
 
 
     @GetMapping("/chefe/{chefeId}")
@@ -39,15 +49,14 @@ public class PratosController {
     public ResponseEntity<List<ListarPratosChefes>> listarTodos() {
         return ResponseEntity.ok(pratosService.listaPratos(null));
     }
-//
-//    // Rota para o chefe autenticado ver os próprios pratos
-//    @GetMapping("/pratos/meus-pratos")
-//    public ResponseEntity<List<ListarPratosChefes>> meusPratos(@AuthenticationPricipal, UserDetais usuariologado) {
-  //  String email = usuarioLogado.getUsername()
-    // chefe chefe = cheferepository.findyByemail(email)
-    //  lança exececao
-//        return ResponseEntity.ok(pratosService.listaPratos(chefe.getId    ));
-//    }
+
+
+    @GetMapping("/pratos/meus-pratos")
+    public ResponseEntity<List<ListarPratosChefes>> meusPratos(@AuthenticationPrincipal UserDetails usuarioLogado) {
+     ChefeModel chefe = chefeRepository.findByEmail(usuarioLogado.getUsername())
+             .orElseThrow(ChefeEception::new);
+        return ResponseEntity.ok(pratosService.listaPratos(chefe.getId()));
+    }
 
     @PutMapping
     public ResponseEntity<CadastratPratosDtoResponse> updatePratos(@RequestBody CadastraPratosDtoRequest dto, Integer id){
