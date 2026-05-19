8d0f068da14f7c615297372a8c3df14dfca24a68
cauacouto <cauacouto022@gmail.com>
2026-05-18 22:31:28 -0300

    feat: update UsuarioMapper to use RegisterRequestDto and map password correctly

--- src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
+++ src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
@@ -6,11 +6,9 @@ import com.couto.chefe_api.User.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
--- src/main/java/com/couto/chefe_api/Mapper/UsuarioMapper.java
+++ src/main/java/com/couto/chefe_api/Mapper/UsuarioMapper.java
@@ -1,9 +1,6 @@
 package com.couto.chefe_api.Mapper;
 
-import com.couto.chefe_api.Dtos.EnderecoRequestDto;
-import com.couto.chefe_api.Dtos.EnderecoResponseDto;
-import com.couto.chefe_api.Dtos.UserRequestDto;
-import com.couto.chefe_api.Dtos.UserResponseDto;
+import com.couto.chefe_api.Dtos.*;
 import com.couto.chefe_api.User.UserModel;
 import com.couto.chefe_api.domin.EnderecoModel;
 import org.mapstruct.Mapper;
@@ -16,7 +13,8 @@ public interface UsuarioMapper {
     UserResponseDto toDto(UserModel model);
 
     @Mapping(target = "endereco",ignore = true)
-    UserModel toModel(UserRequestDto dto,String senhaCriptografada);
+    @Mapping(target = "password",source = "senhaCriptografada")
+    UserModel toModel(RegisterRequestDto dto, String senhaCriptografada);
 
     EnderecoModel toModel(EnderecoRequestDto dto);
 
