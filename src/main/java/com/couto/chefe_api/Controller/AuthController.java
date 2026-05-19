41e77cb3c2ad82d6888dc8b8147e2589b1837ffd
cauacouto <cauacouto022@gmail.com>
2026-05-19 15:41:56 -0300

    feat: update register endpoint to handle multipart file for user registration

--- src/main/java/com/couto/chefe_api/Controller/AuthController.java
+++ src/main/java/com/couto/chefe_api/Controller/AuthController.java
@@ -6,11 +6,10 @@ import com.couto.chefe_api.Dtos.RegisterRequestDto;
 import com.couto.chefe_api.Security.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,11 +20,11 @@ public class AuthController {
 
 
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDto dto){
+    @PostMapping(value = "/register",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> register(@RequestPart("dados") RegisterRequestDto dto, @RequestParam MultipartFile foto){
        return switch (dto.getTipoUsuario()){
            case CLIENTE -> ResponseEntity.status(HttpStatus.CREATED).body(loginService.RegisterUsuario(dto));
-           case CHEFE ->  ResponseEntity.status(HttpStatus.CREATED).body(loginService.registerChefe(dto));
+           case CHEFE ->  ResponseEntity.status(HttpStatus.CREATED).body(loginService.registerChefe(dto,foto));
        };
     }
 
