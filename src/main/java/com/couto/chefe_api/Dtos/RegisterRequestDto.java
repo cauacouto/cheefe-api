76bfe966b4fdaefddf70a4a66805deb51777824c
cauacouto <cauacouto022@gmail.com>
2026-05-18 22:31:01 -0300

    feat: add RegisterRequestDto for user registration with validation constraints

---
+++ src/main/java/com/couto/chefe_api/Dtos/RegisterRequestDto.java
@@ -0,0 +1,43 @@
+package com.couto.chefe_api.Dtos;
+
+import com.couto.chefe_api.enums.Especialidades;
+import com.couto.chefe_api.enums.Senioridade;
+import com.couto.chefe_api.enums.TipoUsuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDto {
+
+
+
+    @NotNull
+    private TipoUsuario tipoUsuario;
+
+
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+
+    private List<EnderecoRequestDto> endereco;
+
+    private String nacionalidade;
+    private Especialidades especialidade;
+    private Senioridade senioridade;
+    private Integer anosDeExperiencia;
+    private String idiomas;
+    private BigDecimal valorHora;
+}
