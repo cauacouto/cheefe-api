040b0f374ab881b1afb83382915b5a5235e1bdf7
cauacouto <cauacouto022@gmail.com>
2026-05-18 22:30:37 -0300

    fix: correct typo in DadosLoginDto field name from 'passaword' to 'password'

--- src/main/java/com/couto/chefe_api/Dtos/DadosLoginDto.java
+++ src/main/java/com/couto/chefe_api/Dtos/DadosLoginDto.java
@@ -1,11 +1,8 @@
 package com.couto.chefe_api.Dtos;
 
-import lombok.Getter;
-import lombok.Setter;
-
 
 public record DadosLoginDto (
         String email,
-        String passaword
+        String password
 ) {
 }
