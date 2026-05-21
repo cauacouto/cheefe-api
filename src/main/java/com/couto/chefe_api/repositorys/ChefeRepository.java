807bc113c8533ac91e7c05b350e7dd050b7eafa5
cauacouto <cauacouto022@gmail.com>
2026-05-20 21:12:45 -0300

    feat: add query findByFiltros, findMaisAgendados

--- src/main/java/com/couto/chefe_api/repositorys/ChefeRepository.java
+++ src/main/java/com/couto/chefe_api/repositorys/ChefeRepository.java
@@ -1,9 +1,14 @@
 package com.couto.chefe_api.repositorys;
 
 import com.couto.chefe_api.domin.ChefeModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,6 +24,20 @@ public interface ChefeRepository extends JpaRepository<ChefeModel, UUID> {
     List<ChefeModel> findByDisponivelTrue();
 
     Optional<ChefeModel> findByEmail(String usuarioLogado);
+
+
+@Query ("SELECT c FROM ChefeModel c WHERE " +
+           "(:valorMinimo IS NULL OR c.valorHora >= :valorMinimo) AND " +
+           "(:valorMaximo IS NULL OR c.valorHora <= :valorMaximo)")
+    Page<ChefeModel> findByFiltros(
+            @Param("valorMinimo") BigDecimal valorMinimo,
+            @Param("valorMaximo") BigDecimal valorMaximo,
+            Pageable pageable);
+
+
+    @Query("SELECT c FROM ChefeModel c LEFT JOIN c.agendamentos a " +
+            "GROUP BY c ORDER BY COUNT(a) DESC")
+    Page<ChefeModel> findMaisAgendados(Pageable pageable);
 
 
 }
