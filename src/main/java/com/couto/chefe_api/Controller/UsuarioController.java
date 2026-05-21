e63e4eceb1894fab43aa54d0b1de11bb3c16325b
cauacouto <cauacouto022@gmail.com>
2026-05-20 21:07:56 -0300

    feat: add endereco service

--- src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
+++ src/main/java/com/couto/chefe_api/Controller/UsuarioController.java
@@ -1,14 +1,20 @@
 package com.couto.chefe_api.Controller;
 
+import com.couto.chefe_api.Dtos.EnderecoRequestDto;
+import com.couto.chefe_api.Dtos.EnderecoResponseDto;
 import com.couto.chefe_api.Dtos.UserRequestDto;
 import com.couto.chefe_api.Dtos.UserResponseDto;
+import com.couto.chefe_api.Services.EnderecoService;
 import com.couto.chefe_api.User.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,6 +23,7 @@ import java.util.UUID;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final EnderecoService enderecoService;
 
 
     @PutMapping
@@ -42,5 +49,19 @@ public ResponseEntity<Void> deletar (@PathVariable UUID id){
         usuarioService.deletar(id);
         return ResponseEntity.noContent().build();
 }
+
+
+    @GetMapping("/enderecos")
+    public ResponseEntity<List<EnderecoResponseDto>> listarEnderecos(Authentication authentication) {
+        return ResponseEntity.ok(enderecoService.listarEnderecos(authentication.getName()));
+    }
+
+    @PostMapping("/enderecos")
+    public ResponseEntity<EnderecoResponseDto> adicionarEndereco(
+            @RequestBody EnderecoRequestDto dto,
+            Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(enderecoService.adicionarEndereco(dto, authentication.getName()));
+    }
 
 }
