31c8a59ae0d713068a21d6274f8e06e842e817fd
cauacouto <cauacouto022@gmail.com>
2026-05-20 21:11:17 -0300

    feat: add annotation

--- src/main/java/com/couto/chefe_api/ChefeApiApplication.java
+++ src/main/java/com/couto/chefe_api/ChefeApiApplication.java
@@ -2,8 +2,10 @@ package com.couto.chefe_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ChefeApiApplication {
 
 	public static void main(String[] args) {
